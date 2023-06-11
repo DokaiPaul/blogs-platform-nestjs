9d71b98867055f444ffaed3684470ef35a8431a9
DokaiPaul <dokaypaul@gmail.com>
2023-06-11 18:12:57 +0300

    feat: add authentication for comments router. Also, implement endpoint to get comment by id

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,6 +5,9 @@ import { UsersRepository } from '../users/infrastructure/users.repository';
 import * as bcrypt from 'bcrypt';
 import { v4 as uuidv4 } from 'uuid';
 import { ActiveSessionService } from '../devices/active.sessions.service';
+import { JwtPayload } from 'jsonwebtoken';
+import { jwtConstants } from './constants';
+import { ActiveSessionRepository } from '../devices/active.sessions.repository';
 
 @Injectable()
 export class AuthService {
@@ -13,6 +16,7 @@ export class AuthService {
     private JwtService: JwtService,
     private UserRepository: UsersRepository,
     private ActiveSessionService: ActiveSessionService,
+    private ActiveSessionRepository: ActiveSessionRepository,
   ) {}
 
   async validateUser(loginOrEmail: string, password: string) {
@@ -33,17 +37,73 @@ export class AuthService {
       userId,
       ip,
       title,
-      lastActivateDate: new Date().toISOString(),
-      tokenExpirationDate: 'some date',
+      tokenExpirationDate: '',
+      lastActivateDate: '',
     };
+
+    const { accessToken, refreshToken } = await this.generateTokens(
+      userId,
+      sessionInfo.deviceId,
+    );
+
+    const parsedRToken = (await this.JwtService.decode(
+      refreshToken,
+    )) as JwtPayload;
+    if (!parsedRToken.exp || !parsedRToken.iat) return null;
+
+    sessionInfo.tokenExpirationDate = new Date(
+      new Date(0).setUTCSeconds(parsedRToken.exp),
+    ).toISOString();
+    sessionInfo.lastActivateDate = new Date(
+      new Date(0).setUTCSeconds(parsedRToken.iat),
+    ).toISOString();
 
     const result = await this.ActiveSessionService.addDevice(sessionInfo);
     if (!result) return null;
 
-    return result;
+    return { accessToken, refreshToken, result };
   }
 
-  async logout(sessionId: string) {}
+  updateRefreshToken(refreshToken: string) {
+    const jwtPayload = this.JwtService.decode(refreshToken);
+    if (!jwtPayload || typeof jwtPayload === 'string') return null;
 
-  async killAllSessions(sessionId: string) {}
+    const isUpdated = this.ActiveSessionService.updateRefreshToken(jwtPayload);
+    if (!isUpdated) return null;
+
+    return isUpdated;
+  }
+
+  async logout(deviceId: string) {
+    return this.ActiveSessionRepository.deleteDeviceById(deviceId);
+  }
+
+  async killAllSessions(sessionId: string) {
+    return;
+  }
+
+  async generateTokens(userId: string, deviceId: string) {
+    const accessToken = this.JwtService.sign(
+      {
+        sub: userId,
+      },
+      {
+        secret: jwtConstants.ACCESS_SECRET,
+        expiresIn: jwtConstants.ACCESS_TIME_EXPIRATION,
+      },
+    );
+
+    const refreshToken = this.JwtService.sign(
+      {
+        deviceId,
+        userId,
+      },
+      {
+        secret: jwtConstants.REFRESH_SECRET,
+        expiresIn: jwtConstants.REFRESH_TIME_EXPIRATION,
+      },
+    );
+
+    return { accessToken, refreshToken };
+  }
 }
