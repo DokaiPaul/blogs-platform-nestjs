a78cc1b55f56ba05239a26ff566983417fade88e
DokaiPaul <dokaypaul@gmail.com>
2023-06-12 19:38:04 +0300

    fix: change import of cookie parser. Hope it'll resolve the troubles with Vercel deployment

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import {
   HttpExceptionFilter,
   validationPipesOptions,
 } from './exception.filter';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
