076f98bede62719531bf79d54cd140ec58b8b184
DokaiPaul <dokaypaul@gmail.com>
2023-06-16 17:35:35 +0300

    feat: add two collections to be cleared with delete 'all-data' endpoint

--- src/testing/testing.controller.ts
+++ src/testing/testing.controller.ts
@@ -13,24 +13,38 @@ import {
   CommentDocument,
 } from '../blogs/infrastructure/comments.schema';
 import { User, UserDocument } from '../users/infrastructure/users.schema';
+import {
+  PasswordRecovery,
+  PasswordRecoveryDocument,
+} from '../users/infrastructure/password.recovery.schema';
+import {
+  ActiveSession,
+  ActiveSessionDocument,
+} from '../devices/active.sessions.model';
 
-@Controller('testing/all-data')
+@Controller('testing')
 export class TestingController {
   constructor(
     @InjectModel(Blog.name) private BlogModel: Model<BlogDocument>,
     @InjectModel(Post.name) private PostModel: Model<PostDocument>,
     @InjectModel(Comment.name) private CommentModel: Model<CommentDocument>,
     @InjectModel(User.name) private UserModel: Model<UserDocument>,
+    @InjectModel(PasswordRecovery.name)
+    private PasswordRecoveryModel: Model<PasswordRecoveryDocument>,
+    @InjectModel(ActiveSession.name)
+    private ActiveSessionModel: Model<ActiveSessionDocument>,
   ) {}
 
   @HttpCode(204)
-  @Delete()
+  @Delete('all-data')
   async clearAllData() {
     try {
       await this.BlogModel.deleteMany({});
       await this.PostModel.deleteMany({});
       await this.CommentModel.deleteMany({});
       await this.UserModel.deleteMany({});
+      await this.PasswordRecoveryModel.deleteMany({});
+      await this.ActiveSessionModel.deleteMany({});
 
       return;
     } catch (e) {
